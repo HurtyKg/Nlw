7420662ee697b17ef80dd134eddd03c25a7b1426
Enzo Oliveira <94401783+HurtyKg@users.noreply.github.com>
2023-05-24 14:49:48 -0300

    changes commit

--- web/src/components/Copyright.tsx
+++ web/src/components/Copyright.tsx
@@ -1,14 +1,14 @@
 export function Copyright() {
   return (
     <div className="text-sm leading-relaxed text-gray-200">
-      Feito com 💜 no NLW da{' '}
+      Feito com 💙{' '}
       <a
         target="_blank"
         rel="noreferrer"
         className="underline hover:text-gray-100"
-        href="https://rocketseat.com.br"
+        href="https://www.linkedin.com/in/enzo-oliveira-a18344229/"
       >
-        Rocketseat
+        Linkedin
       </a>
     </div>
   )
